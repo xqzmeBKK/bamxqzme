d9f3c2928a9ab188160da205fc815e31294fe837
xqzme <bamxqzme@gmail.com>
2019-09-10 22:13:08 +0200

    Add files via upload

--- BAMxqzme.lua
+++ BAMxqzme.lua
@@ -24,13 +24,15 @@ SlashCmdList["BAMXQZME"] = function(message)
     BAMxqzmeSettings = "GROUP"
   elseif cmd[1] == "raid" then
     BAMxqzmeSettings = "RAID"
-  else
+  elseif cmd[1] == "self" then
+    BAMxqzmeSettings = "SELF"
+	else
     DEFAULT_CHAT_FRAME:AddMessage("|cff33ffccBAM|rxqzme usage:")
-    DEFAULT_CHAT_FRAME:AddMessage("/bam [say, yell, group, raid]")
+    DEFAULT_CHAT_FRAME:AddMessage("/bam [say, yell, group, raid, self]")
   end
 
   if not BAMxqzmeSettings then 
-    BAMxqzmeSettings = "YELL" 
+    BAMxqzmeSettings = "SELF" 
   end
   DEFAULT_CHAT_FRAME:AddMessage("-> you are now spamming your crits in '" .. string.lower(BAMxqzmeSettings) .. "'")
   
@@ -57,13 +59,20 @@ local OutputMessageHeal = "I healed you big time!"
 		-- damage
 		if (type == "SPELL_DAMAGE") and critical and (sourceGUID == playerGUID) then
 			if amount >= CritAmountDmg then
+			if BAMxqzmeSettings == "SELF" then
+				DEFAULT_CHAT_FRAME:AddMessage(OutputMessageDmg.." "..spellName.." - "..format_thousand(amount))
+				script=PlaySoundFile("Interface\\AddOns\\BAMxqzme\\bam.ogg", "Dialog")
+				else
 				if EveryoneGitInHereDmg == 1 then
-					SendChatMessage(OutputMessageDmg.." "..spellName.." - "..format_thousand(amount), BAMxqzmeSettings ,nil);
+					if BAMxqzmeSettings == "SAY" then
+						SendChatMessage(OutputMessageDmg.." "..spellName.." - "..format_thousand(amount), BAMxqzmeSettings ,nil);
+					end
 				else
 					print(OutputMessageDmg.." | "..spellName.." - "..format_thousand(amount))
 				end
-				script=PlaySoundFile("Interface\\AddOns\\BAMxqzme\\bam.ogg")
+				script=PlaySoundFile("Interface\\AddOns\\BAMxqzme\\bam.ogg", "Dialog")
 			end
+		end
 		end
 		
 
@@ -72,11 +81,13 @@ local OutputMessageHeal = "I healed you big time!"
 		if (type == "SPELL_HEAL") and critical and (sourceGUID == playerGUID) then
 			if amount >= CritAmountHeal then
 				if EveryoneGitInHereHeal == 1 then
-					SendChatMessage(OutputMessageHeal.." "..destName.." - "..spellName.." - "..format_thousand(amount), BAMxqzmeSettings ,nil);
-				else
+					if BAMxqzmeSettings ~= "SAY" then
+						SendChatMessage(OutputMessageHeal.." "..destName.." - "..spellName.." - "..format_thousand(amount), BAMxqzmeSettings ,nil);
+					end
+				elseif BAMxqzmeSettings ~= "SELF" then
 					print(OutputMessageHeal.." | "..destName.." with "..spellName.." for "..format_thousand(amount))
 				end
-				script=PlaySoundFile("Interface\\AddOns\\BAMxqzme\\bam.ogg")
+				script=PlaySoundFile("Interface\\AddOns\\BAMxqzme\\bam.ogg", "Dialog")
 			end
 		end
   end
